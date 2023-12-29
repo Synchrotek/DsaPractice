11d6ce445f54dc268f926c03580fe03d7d8322f8
Satyajit Nayak <satyajitnayak348@gmail.com>
2023-12-29 22:29:18 +0530

    S3arch1ng

--- S3arch1ng.cpp
+++ S3arch1ng.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size, int key)
+int binarySearch(int arr[], int n, int key)
 {
-    int start = 0;
-    int end = size - 1;
-
-    int mid = (start + end) / 2;
-
-    while (start <= end)
+    int s = 0, e = n; // starting, sending
+    while (s <= e)
     {
+        int mid = (s + e) / 2;
         if (arr[mid] == key)
         {
             return mid;
         }
-        if (key > arr[mid])
+        else if (arr[mid] > key)
         {
-            start = mid + 1;
+            e = mid - 1;
         }
-        if (key < arr[mid])
+        else
         {
-            end = mid - 1;
+            s = mid + 1;
         }
-        mid = (start + end) / 2;
     }
     return -1;
 }
 
 int main()
 {
-    int even[6] = {2, 4, 6, 8, 12, 18};
-    int odd[5] = {3, 8, 11, 14, 16};
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
 
-    int evenIndex = binarySearch(even, 6, 18);
-    int oddIndex = binarySearch(odd, 6, 11);
-    cout << "Index of 12 is: " << evenIndex << endl;
-    cout << "Index of 12 is: " << oddIndex << endl;
+    // Element to search
+    int key;
+    cin >> key;
+
+    cout << binarySearch(arr, N, key);
 
     return 0;
 }
