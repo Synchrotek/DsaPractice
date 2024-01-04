a90d61cd34c26ab87955c66b7eaad4ab088c53b0
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-01-04 06:02:24 +0530

    getMinMax in arr

--- DSA-problems/Set-1-problems/problem002.cpp
+++ DSA-problems/Set-1-problems/problem002.cpp
@@ -1,2 +1,44 @@
 // Find minimum and maximum element in an array
 // ProblemLink: https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1
+
+#include <iostream>
+#include <utility>
+using namespace std;
+
+pair<long long, long long> getMinMax(long long a[], int n)
+{
+    pair<long long, long long> p = {-1, -1};
+    if (a == NULL || n == 0)
+    {
+        return p;
+    }
+    int min = a[0];
+    int max = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+    p = {min, max};
+    return p;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    pair<long long, long long> p = getMinMax(a, n);
+    cout << "Min: " << p.first << endl;
+    cout << "Max: " << p.second << endl;
+
+    return 0;
+}
