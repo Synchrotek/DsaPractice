5b28baa076146923e69b734bc6c40ebb530567a0
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-01-06 22:04:13 +0530

    heap practice

--- DSA-problems/Set-1-problems/problem003.cpp
+++ DSA-problems/Set-1-problems/problem003.cpp
@@ -7,10 +7,6 @@ using namespace std;
 class Solution
 {
 public:
-    // arr : given array
-    // l : starting index of the array i.e 0
-    // r : ending index of the array i.e size-1
-    // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k)
     {
         // code here
