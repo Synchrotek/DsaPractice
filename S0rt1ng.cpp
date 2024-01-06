fde706dd03e1392c2f20a90ffd3fe09bdab21c53
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-01-06 22:07:40 +0530

    s0rtPractice

--- S0rt1ng.cpp
+++ S0rt1ng.cpp
@@ -1,72 +1,30 @@
+// Insertion sort practice
 #include <iostream>
 using namespace std;
-
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
-        int mid = (l + r) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-
-        merge(arr, l, mid, r);
-    }
-}
-
-void merge(int arr[], int l, int mid, int r)
-{
-    int n1 = mid - l + 1;
-    int n2 = r - mid;
-    int a[n1], b[n2];
-
-    for (int i = 0; i < n1; i++)
-    {
-        a[i] = arr[l + i];
-    }
-    for (int j = 0; j < n2; j++)
-    {
-        b[j] = arr[mid + 1 + j];
-    }
-
-    int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2)
-    {
-        if (a[i] < b[j])
-        {
-            arr[k] = a[i];
-            k++, i++;
-        }
-        else
-        {
-            arr[k] = b[j];
-            k++, j++;
-        }
-    }
-
-    while (i < n1)
-    {
-        arr[k] = a[i];
-        k++, i++;
-    }
-
-    while (j < n2)
-    {
-        arr[k] = b[j];
-        k++, j++;
-    }
-}
 
 int main()
 {
     int n;
     cin >> n;
+
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    mergeSort(arr, 0, n - 1);
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (arr[j] > current && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
