974e4d243a170d8aa9000b11beb10e7c274e95f0
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-01-10 17:20:27 +0530

    postfixEval

--- Structured-way/ST4ck/problems/PreInPostFix.cpp
+++ Structured-way/ST4ck/problems/PreInPostFix.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 using namespace std;
 
+// prefix -------------------------------------
 int prefixEval(string s)
 {
     stack<int> st;
@@ -34,7 +35,7 @@ int prefixEval(string s)
                 st.push(op1 / op2);
                 break;
             case '^':
-                st.push(op1 ^ op2);
+                st.push(pow(op1, op2));
                 break;
             }
         }
@@ -42,10 +43,54 @@ int prefixEval(string s)
     return st.top();
 }
 
+// postfix ------------------------------------
+int postfixEval(string s)
+{
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            st.push(s[i] - '0');
+        }
+        else
+        {
+            int op2 = st.top();
+            st.pop();
+            int op1 = st.top();
+            st.pop();
+
+            switch (s[i])
+            {
+            case '+':
+                st.push(op1 + op2);
+                break;
+            case '-':
+                st.push(op1 - op2);
+                break;
+            case '*':
+                st.push(op1 * op2);
+                break;
+            case '/':
+                st.push(op1 / op2);
+                break;
+            case '^':
+                st.push(pow(op1, op2));
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+
+// Drive code ---------------------------------
 int main()
 {
-    string str = ("-+7*45+20");
-    cout << prefixEval(str) << endl;
+    string prefixStr = ("-+7*45+20");
+    string postfixStr = ("46+2/5*7+");
+
+    cout << prefixEval(prefixStr) << endl;
+    cout << postfixEval(postfixStr) << endl;
 
     return 0;
 }
