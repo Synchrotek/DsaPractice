da8fc5c533bd4191bd9361f6cab9ae4988e8838b
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-01-12 22:30:59 +0530

    In2PrePostFix

---
+++ Structured-way/ST4ck/problems/In2PrePostFix.cpp
@@ -0,0 +1,65 @@
+// ----------------------------------------
+// NOT COMPLETED YET
+// ----------------------------------------
+// ----------------------------------------
+
+#include <iostream>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+int prec(char c)
+{
+    if (c == '^')
+    {
+        return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int InToPreFix(string s)
+{
+    stack<char> st;
+    string res;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            res += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                res += st.top();
+                st.pop();
+            }
+            if (!st.empty())
+            {
+                st.pop();
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
