37229538bb5b7a981dbdf3ffcd84482221e7bc7a
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-15 22:14:40 +0530

    setup 4_sort012

--- DSA-450Problems/Array/4_sort012.cpp
+++ DSA-450Problems/Array/4_sort012.cpp
@@ -4,8 +4,24 @@
 #include <iostream>
 using namespace std;
 
+class Solution
+{
+public:
+    void segregateElements(int arr[], int n)
+    {
+        // Your code goes here
+    }
+};
+
 int main()
 {
+    int N; // Size of arary
+    int arr[N];
+    for (int i = 0; i < 0; i++)
+        cin >> arr[i];
+
+    Solution s1;
+    s1.segregateElements(arr, N);
 
     return 0;
 }
