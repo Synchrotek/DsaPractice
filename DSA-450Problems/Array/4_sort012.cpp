3a1b571d72ba4d807a75ee9c46dc30e17bf1bee9
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-15 23:09:17 +0530

    4_sort012

--- DSA-450Problems/Array/4_sort012.cpp
+++ DSA-450Problems/Array/4_sort012.cpp
@@ -7,9 +7,31 @@ using namespace std;
 class Solution
 {
 public:
-    void segregateElements(int arr[], int n)
+    // Best Approach - DNF algo --------------
+    void sort012(int arr[], int n)
     {
-        // Your code goes here
+        int low = 0, mid = 0, high = n - 1, temp;
+        while (mid <= high)
+        {
+            if (arr[mid] == 0)
+            {
+                temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++, mid++;
+            }
+            else if (arr[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
     }
 };
 
@@ -21,7 +43,30 @@ int main()
         cin >> arr[i];
 
     Solution s1;
-    s1.segregateElements(arr, N);
+    s1.sort012(arr, N);
 
     return 0;
 }
+
+/* Better Approach ---------------------------
+    void sort012(int arr[], int n)
+    {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == 0)
+                cnt0++;
+            else if (arr[i] == 1)
+                cnt1++;
+            else
+                cnt2++;
+        }
+
+        for (int i = 0; i < cnt0; i++)
+            arr[i] = 0;
+        for (int i = cnt0; i < cnt0 + cnt1; i++)
+            arr[i] = 1;
+        for (int i = cnt0 + cnt1; i < n; i++)
+            arr[i] = 2;
+    }
+*/
